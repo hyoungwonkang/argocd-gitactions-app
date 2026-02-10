8f79ed8a7567b52f0fe4c5c3f77dac8434a626ec
hyoungwonkang <hwplus@gmail.com>
2026-02-10 14:06:01 +0900

    문자열 변경

--- src/main/java/com/example/gitactions/controller/ArgocdGitactionsAppController.java
+++ src/main/java/com/example/gitactions/controller/ArgocdGitactionsAppController.java
@@ -18,6 +18,6 @@ public class ArgocdGitactionsAppController {
       e.printStackTrace();
     }
     // 배포된 버전을 확인하기 위해 v1, v2 등으로 텍스트를 변경하며 실습합니다.
-    return "Hello from CI/CD Pipeline (v1) - Host: " + hostName;
+    return "Hello from CI/CD Pipeline (v2) - Host: " + hostName;
   }
 }
